0d0401eb6385a98cd7e9373d682252271a10dd64
Bruno Freires <freiresdasilvabrunoeduardo@gmail.com>
2025-11-17 17:34:18 -0300

    problema das letras em ligth resolvidos

--- main.js
+++ main.js
@@ -4,4 +4,6 @@ const body = document.body;
 trilho.addEventListener('click', () => {
   trilho.classList.toggle('dark'); // animação do botão
   body.classList.toggle('light'); // alterna o modo claro/escuro
+  <body class="light">  </body>//"dark"//
+
 });
