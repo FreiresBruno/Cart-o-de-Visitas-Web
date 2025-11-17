e2e14e4761e175eb22b81df30ef0f2e1141eb816
Bruno Freires <freiresdasilvabrunoeduardo@gmail.com>
2025-11-17 14:16:06 -0300

    modo claro adicionado

---
+++ main.js
@@ -0,0 +1,7 @@
+const trilho = document.getElementById('trilho');
+const body = document.body;
+
+trilho.addEventListener('click', () => {
+  trilho.classList.toggle('dark'); // animação do botão
+  body.classList.toggle('light'); // alterna o modo claro/escuro
+});
